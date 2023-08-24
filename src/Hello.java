f3df205b37cf7c8cc0c9dc925442ecf96e667406
SofijaCa <sofija121983@gmail.com>
2023-08-24 15:44:35 +0200

    myfirstinelegi

---
+++ src/Hello.java
@@ -0,0 +1,23 @@
+public class Hello{
+    public static void main(String[] args) {
+    System.out.println("Hello, World !!!");
+        String name;
+        name = "Sofija";
+        System.out.println("Hello " +name);
+        System.out.println("Good bye " +name);
+
+}
+
+    public static void sayHello(String name) {
+
+        System.out.println("Hello " +name);
+
+    }
+    public static void sayGoodBye(String name) {
+        System.out.println("Good bye " +name);
+    }
+
+
+
+     }
+
